2e48c5f811b6dc723f9472411cfd6daebc370c3c
Jelle Smet <jelle.smet@tomtom.com>
2024-09-22 19:20:04 +0200

    adding support for age and checking if local file already exists

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A CLI tool to sync a Dropbox folder to a folder on the local file system. It
 is not a bi-directional sync.
 
+THIS IS A WIP
+
--- dbsync/__init__.py
+++ dbsync/__init__.py
@@ -5,14 +5,28 @@
 #
 
 
+import os.path
 from pathlib import Path
 
+import arrow
 import click
 import dropbox
 from dropbox.files import FolderMetadata
 
 
-def traverse_folder(dbx, folder_name, cursor=None):
+def file_age_ok(dbx_file, max_age) -> bool:
+    """
+    Validates if the dropbox file is older than `max_age` or not.
+    """
+    total_seconds = (
+        arrow.utcnow() - arrow.get(dbx_file.client_modified)
+    ).total_seconds()
+    if (total_seconds / 60 / 60 / 24) > max_age:
+        return False
+    return True
+
+
+def traverse_dbx_folder(dbx, folder_name, cursor=None):
     """
     Recursively traverses the Dropbox folder `folder_name` and yields all
     found files.
@@ -25,7 +39,7 @@ def traverse_folder(dbx, folder_name, cursor=None):
 
         for entry in response.entries:
             if isinstance(entry, FolderMetadata):
-                for recursed_entry in traverse_folder(dbx, entry.path_display):
+                for recursed_entry in traverse_dbx_folder(dbx, entry.path_display):
                     yield recursed_entry
             else:
                 yield entry
@@ -36,7 +50,13 @@ def traverse_folder(dbx, folder_name, cursor=None):
         cursor = response.cursor
 
 
+def local_remote_hash_differs(local_path, dbx_file):
+    print(dbx_file)
+
+
 def download_file(dbx, remote_path, local_path):
+    Path(f"{os.path.dirname(local_path)}").mkdir(parents=True, exist_ok=True)
+
     print(
         f'Copying dropbox://{remote_path} to local://{local_path.lstrip("/")}: ',
         end="",
@@ -50,7 +70,9 @@ def download_file(dbx, remote_path, local_path):
 @click.command(
     name="dbsync", help="A CLI sync a Dropbox folder to the local file system."
 )
-@click.option("--token", required=True, envvar="DBSYNC_REFRESH_TOKEN", help="The Dropbox token")
+@click.option(
+    "--token", required=True, envvar="DBSYNC_REFRESH_TOKEN", help="The Dropbox token"
+)
 @click.option(
     "--app-key", required=True, envvar="DBSYNC_APP_KEY", help="The Dropbox token"
 )
@@ -69,14 +91,23 @@ def download_file(dbx, remote_path, local_path):
     envvar="DBSYNC_LOCAL_FOLDER",
     help="The local folder to sync to.",
 )
-def main(token, app_key, app_secret, dropbox_folder, local_folder):
+@click.option(
+    "--max-days",
+    required=False,
+    default="31",
+    envvar="DBSYNC_MAX_DAYS",
+    type=int,
+    help="The the maximum file age expressed in number of days.",
+)
+def main(token, app_key, app_secret, dropbox_folder, local_folder, max_days):
     dbx = dropbox.Dropbox(
         oauth2_refresh_token=token, app_key=app_key, app_secret=app_secret
     )
-    for entry in traverse_folder(dbx, dropbox_folder):
-        path = Path(entry.path_display)
-        Path(f"{local_folder}/{path.parent}").mkdir(parents=True, exist_ok=True)
-        download_file(dbx, entry.path_display, f"{local_folder}/{entry.path_display}")
+    for entry in traverse_dbx_folder(dbx, dropbox_folder):
+        if file_age_ok(entry, max_days):
+            local_file_name = os.path.abspath(f"{local_folder}/{entry.path_display}")
+            if not os.path.isfile(local_file_name):
+                download_file(dbx, entry.path_display, local_file_name)
 
 
 if __name__ == "__main__":
